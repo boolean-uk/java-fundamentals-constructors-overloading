6fe4ace517c12da9693784af9794b0a376dac49c
PiotrSadolewski <piotr.sadolewski00@gmail.com>
2023-07-05 09:52:51 +0200

    extension exercises added

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -4,27 +4,66 @@ import com.booleanuk.helpers.ExtensionBase;
 
 public class Extension extends ExtensionBase {
     /*
-        Implement the following methods:
+   Implement the following methods:
 
-        1. add, which accepts two floats and returns a float (both floats added together)
+   1. add, which accepts two floats and returns a float (both floats added together)
+   */
+    public float add(float a, float b){
+        return a + b;
+    }
+    /*
 
-        2. add, which accepts two doubles and returns a double (both doubles added together)
+    2. add, which accepts two doubles and returns a double (both doubles added together)
+    */
+    public double add(double a, double b){
+        return a + b;
+    }
+    /*
+    3. subtract, which accepts two floats and returns a float (first float minus second float)
+    */
+    public float subtract(float a, float b){
+        return a - b;
+    }
+    /*
 
-        3. subtract, which accepts two floats and returns a float (first float minus second float)
-
-        4. subtract, which accepts a String and a char and returns a string with all instances of the provided char removed
-
-        5. multiply, which accepts two ints and returns an int (first int multiplied by second int)
-
+    4. subtract, which accepts a String and a char and returns a string with all instances of the provided char removed
+    */
+    public String subtract(String a, char b){
+        return a.replaceAll(Character.toString(b), "");
+    }
+    /*
+    5. multiply, which accepts two ints and returns an int (first int multiplied by second int)
+    */
+    public int multiply(int int1, int int2){
+        return int1 * int2;
+    }
+        /*
         6. multiply, which accepts a string and an int, and returns a string containing the provided string
         as many times as the provided int separated by a comma. E.g.
         multiply("Hello", 3) -> "Hello,Hello,Hello"
+        */
 
-        7. multiply, which accepts an array of Strings that each contain a number, and an int
-        The method should return an array of ints that contain the value of multiplying each String number by the provided int
-        E.g.
-        multiply(["2", "7", "3"], 3) -> [6, 21, 9]
-     */
+    public String multiply(String s, int int1){
+        s = s + ",";
+        s = s.repeat(int1);
+        return s.substring(0, s.length() -1);
+    }
+
+    /*
+    7. multiply, which accepts an array of Strings that each contain a number, and an int
+    The method should return an array of ints that contain the value of multiplying each String number by the provided int
+    E.g.
+    multiply(["2", "7", "3"], 3) -> [6, 21, 9]
+    */
+    public int[] multiply (String[] arr, int int1){
+
+        int[] arrInt = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            arrInt[i] = Integer.parseInt(arr[i]) * int1;
+        }
+
+        return arrInt;
+    }
 
 
 }
