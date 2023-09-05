e844cc6d5c8984742b1981c44aa07f38cb4aa9af
Kevin Huang <krythuang@gmail.com>
2023-09-05 13:27:56 +0200

    Kevin Huang - finished the core and extension

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -26,5 +26,43 @@ public class Extension extends ExtensionBase {
         multiply(["2", "7", "3"], 3) -> [6, 21, 9]
      */
 
+    public float add(float a, float b)
+    {
+        return a + b;
+    }
 
+    public double add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public float subtract(float a, float b)
+    {
+        return a - b;
+    }
+
+    public String subtract(String a, char b)
+    {
+        return a.replaceAll(Character.toString(b), "");
+    }
+
+    public int multiply(int a, int b)
+    {
+        return a * b;
+    }
+
+    public String multiply(String a, int b)
+    {
+        return (a + ',').repeat(b - 1) + a;
+    }
+
+    public int[] multiply(String[] a, int b)
+    {
+        int[] returnArray = new int[a.length];
+        for (int i = 0; i < a.length; i++)
+        {
+            returnArray[i] = Integer.parseInt(a[i]) * b;
+        }
+        return returnArray;
+    }
 }
