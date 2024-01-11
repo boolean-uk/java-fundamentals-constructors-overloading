7e665e495eabb9ff740a30b86802736828a3c56c
Nora Hønnåshagen Hansen <nora.hans1@hotmail.com>
2024-01-11 12:56:57 +0100

    Exercises done

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -25,6 +25,49 @@ public class Extension extends ExtensionBase {
         E.g.
         multiply(["2", "7", "3"], 3) -> [6, 21, 9]
      */
+    public float add(float a, float b)  {
+        return a + b;
+    }
 
+    public double add(double a, double b)   {
+        return a + b;
+    }
 
+    public float subtract(float a, float b) {
+        return a - b;
+    }
+
+    public String subtract(String s, char c)    {
+        StringBuilder resultString = new StringBuilder();
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s.charAt(i) != c) resultString.append(s.charAt(i));
+        }
+        return resultString.toString();
+    }
+
+    public int multiply(int a, int b)   {
+        return a * b;
+    }
+
+    public String multiply(String s, int m) {
+        StringBuilder resultString = new StringBuilder();
+        for(int i = 0; i < m; i++)
+        {
+            resultString.append(s + ",");
+        }
+        return resultString.substring(0,resultString.length()-1);
+    }
+
+    public int[] multiply(String[] sArray, int n)   {
+        int[] resultArray = new int[sArray.length];
+
+        int i = 0;
+        for(String s : sArray)  {
+            int val = Integer.parseInt(s);
+            resultArray[i] = val * n;
+            i++;
+        }
+        return resultArray;
+    }
 }
