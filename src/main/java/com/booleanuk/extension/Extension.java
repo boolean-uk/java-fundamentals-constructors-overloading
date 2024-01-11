0b5778604c8d5c8a81c3e48bb348c99816e186d3
noahlenn <noahlenn@hotmail.com>
2024-01-11 13:19:16 +0100

    Noah Lennestad

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -26,5 +26,55 @@ public class Extension extends ExtensionBase {
         multiply(["2", "7", "3"], 3) -> [6, 21, 9]
      */
 
+    //1
+    public float add(float num1, float num2){
+        return num1 + num2;
+    }
+    //2
+    public double add(double num1, double num2){
+        return num1 + num2;
+    }
+    //3
+    public float subtract(float num1, float num2){
+        return num1 - num2;
+    }
+    //4
+    public String subtract(String num1, char num2) {
+        String num2AsString = String.valueOf(num2);
+        String num1MinusNum2 = num1.replace(num2AsString, "");
 
+        return num1MinusNum2;
+    }
+
+    //5
+    public int multiply(int num1, int num2){
+        return num1 * num2;
+    }
+
+    //6
+    public String multiply(String num1, int num2){
+        StringBuilder stringAndComma = new StringBuilder();
+        for(int i = 0; i < num2; i++) {
+            stringAndComma.append(num1);
+            stringAndComma.append(",");
+        }
+        stringAndComma.deleteCharAt(stringAndComma.length()-1);
+        return stringAndComma.toString();
+    }
+
+    //7
+    public int[] multiply(String[] num1, int num2){
+        int sum = 0;
+        int[] newArr = new int[num1.length];
+        String temp = "";
+        for(int i = 0; i < num1.length; i++){
+            temp = num1[i];
+            sum = Integer.parseInt(temp) * num2;
+            //num1[i] = Integer.parseInt(num1[i]);
+            newArr[i] = sum;
+
+        }
+        return newArr;
+
+    }
 }
